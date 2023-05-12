62f073546f5580ffbf0f752eac00b91a4834df42
Horashio <Horashi0@users.noreply.github.com>
2023-05-12 22:02:41 +0100

    basic code complete

--- ReversingSensor.ino
+++ ReversingSensor.ino
@@ -1,17 +1,29 @@
 #include "SR04.h"
-#define TRIG_PIN 12
-#define ECHO_PIN 11
+#include <LiquidCrystal.h>
+#define TRIG_PIN 53
+#define ECHO_PIN 51
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 long a;
+String aString;
 
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup() {
    Serial.begin(9600);
+   lcd.clear();
+   lcd.begin(16, 2);
    delay(1000);
 }
 
 void loop() {
+  lcd.setCursor(0, 0);
    a=sr04.Distance();
-   Serial.print(a);
-   Serial.println("cm");
-   delay(1000);
+   lcd.print("Distance: ");
+   lcd.setCursor(10,0);
+   lcd.print(a);
+   aString = String(a);
+   int length = aString.length();
+   lcd.setCursor(length + 10, 0);
+   lcd.print("cm");
+   lcd.print("    ");
+   delay(500);
 }
