b6f0b73ee46ad833a49a81fd2a6486a5833cf341
Horashio <Horashi0@users.noreply.github.com>
2023-06-08 18:05:30 +0100

    final progress commit

--- ReversingSensor.ino
+++ ReversingSensor.ino
@@ -3,7 +3,7 @@
 #include <LiquidCrystal.h>
 
 
-// Define Pins
+// Define Pinsg
 #define TRIG_PIN 53
 #define ECHO_PIN 51
 #define BLUE 3
@@ -16,12 +16,12 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 String aString;
 long a;
 int redValue;
+const int buzzerPin = 5;
 
-int duration = 500;
-int buzzer = 5;
+// array containing the distances
+// int distance[] = {50, 40, 30, 20, 10, 5};
+// int numDistances = sizeof(distance) / sizeof(distance[0]);
 
-int distance[] = {50, 40, 30, 20, 10, 5};
-int numDistances = sizeof(distance) / sizeof(distance[0]);
 
 void setup() {
   // LCD
@@ -33,12 +33,14 @@ void setup() {
   pinMode(RED, OUTPUT);
 
   // Buzzer
-  pinMode (buzzer, OUTPUT) ;
+  pinMode (buzzerPin, OUTPUT) ;
+
+
 }
 
 
 void loop() {
-  
+
   // Distance
   a = sr04.Distance();
   aString = String(a);
@@ -54,34 +56,65 @@ void loop() {
   lcd.print("    ");
   delay(500);
 
-  // Distances
-  for (int i = 0; i < numDistances; i++)
-  {
-    if (a <= distance[i]) {
-      switch (distance[i]) {
-        case 50:
+
+  if (a <= 50 && a > 40) {
+    
+    tone(buzzerPin, 800,30); //send tone
+    
+    digitalWrite(RED, HIGH);
+    
+    digitalWrite(RED, HIGH); 
+    delay(250); 
+    digitalWrite(RED, LOW);
+    delay(250);
+    noTone(buzzerPin);
+  }
+  if (a <= 40 && a > 30) {
+    tone(buzzerPin, 800,30); //send tone
+    
+    digitalWrite(RED, HIGH);
+    
+    digitalWrite(RED, HIGH); 
+    delay(150); 
+    digitalWrite(RED, LOW);
+    delay(150);
+    noTone(buzzerPin);
+  
+  }
+  if (a <= 30 && a > 20) {
+    tone(buzzerPin, 800,30); //send tone
+    
+    digitalWrite(RED, HIGH);
+    
+    digitalWrite(RED, HIGH); 
+    delay(50); 
+    digitalWrite(RED, LOW);
+    delay(50);
+    noTone(buzzerPin);
+  }
+  if (a <= 20 && a > 15) {
+    tone(buzzerPin, 800); //send tone
+    
+    digitalWrite(RED, HIGH);
+    
+    digitalWrite(RED, HIGH); 
+    delay(10); 
+    digitalWrite(RED, LOW);
+    delay(10);
+    noTone(buzzerPin);
+  }
+  if (a < 15) {
+    while (a < 15) {
+      tone(buzzerPin, 800); //send tone
         
-          for (int x = 0; x <100; x++) // When a frequency sound
-          {
-            digitalWrite (buzzer, HIGH) ; //send tone
-            delay (1) ;
-            digitalWrite (buzzer, LOW) ; //no tone
-            delay (1) ;
-          }
-          digitalWrite(RED, HIGH);
-          redValue = 250;  
-          digitalWrite(RED, HIGH); 
-          delay(1000); 
-          digitalWrite(RED, LOW);
-          delay(1000);
-          break;
-      }
-      break;
-    } else {
-      digitalWrite(RED, LOW);
+      digitalWrite(RED, HIGH); 
+      a = sr04.Distance();
     }
   }
-  
+  else {
+  digitalWrite(RED, LOW);
+  noTone(buzzerPin);
+  }
 }
 
 
